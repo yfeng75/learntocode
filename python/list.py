485de920ead9b7c48b269a69fe374a000e6046a1
Yan Feng <fengyan@gmail.com>
2020-04-14 22:12:29 -0400

    Updated on 4.14

---
+++ python/list.py
@@ -0,0 +1,28 @@
+# list_1=[]
+# list_2=list()
+
+# print("list 1:{}".format(list_1))
+# print("list_2:{}".format(list_2))
+
+# if list_1 == list_2:
+#     print("The lists are equal")
+
+# print(list("The lists are equal"))
+
+# another_even = sorted(even,reverse=True)
+
+# print(another_even is even)
+
+# another_even.sort(reverse=True)
+# print(even)
+even=[2,4,6,8]
+odd=[1,3,5,7,9]
+
+numbers=[even,odd]
+print(numbers)
+
+for number_set in numbers:
+    print(number_set)
+
+    for value in number_set:
+        print(value)
